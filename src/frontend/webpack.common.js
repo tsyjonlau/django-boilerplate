47b17f0a0c9bed17117399c3f6d07fe6c5e16f4e
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-09-26 23:26:21 -0400

    config(frontend): settting up webpack

---
+++ src/frontend/webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }],
+              '@babel/preset-react'
+            ],
+            plugins: ['@babel/plugin-transform-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
+      }
+    ]
+  }
+};
