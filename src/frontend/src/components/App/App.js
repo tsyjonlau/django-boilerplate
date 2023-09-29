378aa5b65fbaa33fd7bc2fd250ae0c8d26d15cbb
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-09-28 22:45:47 -0400

    deps: install redux toolkit

--- src/frontend/src/components/App/App.js
+++ src/frontend/src/components/App/App.js
@@ -25,17 +25,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            {!loading && success && <p>GET /api/sample/ is successful.</p>}
-            <p>Edit <code>src/App.js</code> and save to reload.</p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        {!loading && success && <p>GET /api/sample/ is successful.</p>}
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,10 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'dotenv/config'
+import 'dotenv/config';
 
-import App from './App';
+import App from './components/App';
+import rootReducer from './reducers';
 
 import './index.css';
+
+const store = configureStore({
+  devTools: process.env.NODE_ENV !== 'production',
+  reducer: rootReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
