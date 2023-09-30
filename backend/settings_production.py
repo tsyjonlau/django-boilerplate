fff7ed6c2c306e2ed12b866e2125c01c9e71334d
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-09-30 13:36:21 -0400

    config: setup for render

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -1,4 +1,5 @@
 import django_heroku
+import dj_database_url
 
 from backend.settings_common import *  # noqa: F401,F403
 
@@ -6,6 +7,10 @@ DEBUG = False
 
 # To be modified for safety
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+	"default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 CORS_ALLOWED_ORIGINS = []
 
