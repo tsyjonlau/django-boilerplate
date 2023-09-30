f0fd9c6af74a913cfed6ee23632079e92f80fa02
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-09-30 15:15:07 -0400

    config(settings): put back SECRET_KEY in dev mode

--- backend/settings_common.py
+++ backend/settings_common.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-4_!y#@++oafvch!!enaf&5jd0^q7@7ooqroq_&$98ca6_4zl(n'
-
 # Application definition
 
 INSTALLED_APPS = [
--- backend/settings_development.py
+++ backend/settings_development.py
@@ -7,3 +7,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:3000',
 ]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-4_!y#@++oafvch!!enaf&5jd0^q7@7ooqroq_&$98ca6_4zl(n'
