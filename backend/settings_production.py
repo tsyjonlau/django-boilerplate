bb8b0ab6cb24c42e931d567e2a2aff612e3dc076
Tsy-Jon Leslie Lau <tsyjonlau@gmail.com>
2023-10-05 17:11:34 -0400

    fix(settings): ALLOWED_HOSTS

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -7,7 +7,7 @@ from backend.settings_common import *  # noqa: F401,F403
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://django-react-boilerplate-ilro.onrender.com/'
+    'django-react-boilerplate-ilro.onrender.com'
 ]
 
 DATABASES = {
